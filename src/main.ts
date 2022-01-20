9233ca36fe2efe3f365507aa87f16316d8ebcee3
John Kinoshita <jrkinoshita@gmail.com>
2022-01-19 23:35:33 -0800

    init upload

---
+++ src/main.ts
@@ -0,0 +1,123 @@
+import isEmpty from "validator/lib/isEmpty";
+import isAlpha from "validator/lib/isAlpha";
+
+const form = document.getElementById("form") as HTMLFormElement;
+const inputMessage = document.getElementById(
+  "inputMessage"
+) as HTMLTextAreaElement;
+let inputKey = document.getElementById("inputKey") as HTMLInputElement;
+const results = document.getElementById("results") as HTMLTextAreaElement;
+const encryptBtn = document.getElementById("encryptBtn") as HTMLButtonElement;
+const decryptBtn = document.getElementById("decryptBtn") as HTMLButtonElement;
+const randomKeyGenBtn = document.getElementById(
+  "randomKeyGenBtn"
+) as HTMLButtonElement;
+
+function getRandomKey(): void {
+  inputKey.value = "";
+  let numOfChar = Math.floor(Math.random() * (15 - 3) + 3);
+  for (let i = 0; i < numOfChar; i++) {
+    inputKey.value += String.fromCharCode(
+      Math.floor(Math.random() * (90 - 65) + 65)
+    );
+  }
+}
+
+function getKeyCharCodeArray(): number[] {
+  let keyCharCodeArray = [];
+  const keyValue: string = inputKey.value
+    .trim()
+    .toUpperCase()
+    .replaceAll(/\s+/g, "");
+  inputKey.value = keyValue;
+  for (let i = 0; i < keyValue.length; i++) {
+    let keyCharCode = keyValue.charCodeAt(i);
+    keyCharCodeArray[i] = keyCharCode - 65;
+  }
+  return keyCharCodeArray;
+}
+
+function encryptMessage(): void {
+  let encryptedMessage: string = "";
+  let keyCharCodeArray: number[] = getKeyCharCodeArray();
+  let count = 0;
+  for (let i = 0; i < inputMessage.value.length; i++) {
+    let messageCharCode = inputMessage.value.trim().toUpperCase().charCodeAt(i);
+    if (
+      messageCharCode < "A".charCodeAt(0) ||
+      messageCharCode > "Z".charCodeAt(0)
+    ) {
+      let nonLetterChars = inputMessage.value.charAt(i);
+      encryptedMessage += nonLetterChars;
+    } else {
+      messageCharCode = messageCharCode - 65;
+      let encryptedCharCode =
+        ((keyCharCodeArray[count++ % keyCharCodeArray.length] +
+          messageCharCode) %
+          26) +
+        65;
+      encryptedMessage += String.fromCharCode(encryptedCharCode);
+    }
+  }
+  results.value = encryptedMessage;
+}
+
+function decryptMessage(): void {
+  let encryptedMessage: string = "";
+  let keyCharCodeArray: number[] = getKeyCharCodeArray();
+  for (let i = 0; i < keyCharCodeArray.length; i++) {
+    keyCharCodeArray[i] = 26 - keyCharCodeArray[i];
+  }
+  let count = 0;
+  for (let i = 0; i < inputMessage.value.length; i++) {
+    let messageCharCode = inputMessage.value.trim().toUpperCase().charCodeAt(i);
+    if (
+      messageCharCode < "A".charCodeAt(0) ||
+      messageCharCode > "Z".charCodeAt(0)
+    ) {
+      let nonLetterChars = inputMessage.value.charAt(i);
+      encryptedMessage += nonLetterChars;
+    } else {
+      messageCharCode = messageCharCode - 65;
+      let encryptedCharCode =
+        ((keyCharCodeArray[count++ % keyCharCodeArray.length] +
+          messageCharCode) %
+          26) +
+        65;
+      encryptedMessage += String.fromCharCode(encryptedCharCode);
+    }
+  }
+  results.value = encryptedMessage;
+}
+
+randomKeyGenBtn.onclick = getRandomKey;
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  setSuccessFor(inputMessage, "");
+  setSuccessFor(inputKey, "");
+  if (isEmpty(inputMessage.value, { ignore_whitespace: true })) {
+    setErrorFor(inputMessage, "Message cannot be blank");
+  } else if (isEmpty(inputKey.value, { ignore_whitespace: true })) {
+    setErrorFor(inputKey, "Key cannont be blank");
+  } else if (!isAlpha(inputKey.value)) {
+    setErrorFor(inputKey, "Key must only use alphabetical characters");
+  } else {
+    encryptBtn.onclick = encryptMessage;
+    decryptBtn.onclick = decryptMessage;
+  }
+});
+
+function setErrorFor(input: HTMLElement, message: string): void {
+  const small = input.parentElement!.querySelector("small");
+  input.classList.add("ring-red-600");
+  input.classList.add("ring-2");
+  small!.innerText = message;
+}
+
+function setSuccessFor(input: HTMLElement, message: string): void {
+  const small = input.parentElement!.querySelector("small");
+  inputMessage.classList.remove("ring-red-600");
+  inputKey.classList.remove("ring-red-600");
+  small!.innerText = message;
+}
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{html,js,ts}"],
+  theme: {
+    extend: {
+      colors: {
+        brown: "#2F2519",
+        lightbr: "#4A3F35",
+        orange: "#FA7D09",
+        darkorg: "#FF4301",
+        xlighttan: "#FFE2C8",
+        lighttan: "#CDB7A3",
+        tan: "#857566",
+      },
+      fontFamily: {
+        inter: ["'Inter'", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
